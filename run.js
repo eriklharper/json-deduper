14b275b10454f9f3cefe1bdb43333bab4d7120a0
Erik Harper <eriklharper@gmail.com>
2015-11-19 22:47:13 -0800

    Working example logging results to the console with source, removed records and resulting deduplicated record set

--- run.js
+++ run.js
@@ -1,54 +1,152 @@
 /**
  * JSON Deduper
+ * Dedupes an array of JSON objects by _id and email.  Takes the record with the most recent timestamp in a set of records that have duplicate _ids or emails.
  *
+ * @author eriklharper
  * @param
  * @returns
  */
-var Baby = require('babyparse');
 var fs = require('fs');
 var argv = require('optimist').argv;
 var path = require('path');
 var moment = require('moment');
 var is = require('is_js');
+var _ = require('lodash');
 
 // Args
 var fileName = argv.file;
-var all = argv.all || false;
 
-if (fileName) {
+/**
+ * [dedupeLeads description]
+ * @param  {[type]} dupes [description]
+ * @return {[type]}       [description]
+ */
+function dedupeLeads(dupes) {
+  var dedupedLeads = [];
+  Object.keys(dupes).forEach(function(key) {
+    dedupedLeads.push(getMostRecentLead(dupes[key][0]));
+  });
+  return dedupedLeads;
+}
 
-  var fileContents = fs.readFileSync(path.resolve(__dirname, './workspace/' + fileName)).toString();
-  var results = [];
-  var leads = JSON.parse(fileContents).leads;
+/**
+ * [getMostRecentLead description]
+ * @param  {[type]} leads [description]
+ * @return {[type]}       [description]
+ */
+function getMostRecentLead(leads) {
+  var initialLeadEntryDate = moment(leads[0].entryDate),
+      mostRecentLead = false;
 
-  // Parse the Leads
-  leads.forEach(function(lead, i, leadsArray) {
-
-    // ID is unique?
-    leadsArray.forEach(function(e, i) {
-      console.log(lead._id);
-    });
-
-    // Dedupe Emails
-
-    //
+  leads.forEach(function(lead) {
+    var leadEntryDate = moment(lead.entryDate);
+    if (leadEntryDate.isAfter(initialLeadEntryDate) || leadEntryDate.isSame(initialLeadEntryDate)) {
+      mostRecentLead = lead;
+    }
   });
 
-  // if (results) {
-  //
-  //   // Put parsed contents into the parsed/ dir
-  //   var parsedDir = __dirname + '/workspace/parsed',
-  //       fullPath = parsedDir + '/' + fileName;
-  //
-  //   console.log("\n\nProcessed leads output to: \n\n" + fullPath);
-  //   console.log("\n\nComplete records: " + validRecordCount);
-  //   console.log("Incomplete records: " + invalidRecordCount);
-  //   console.log("Total records: " + (invalidRecordCount + validRecordCount));
-  //
-  //   if (!fs.existsSync(parsedDir)){
-  //     fs.mkdirSync(parsedDir, '0777');
-  //   }
-  //
-  //   fs.writeFileSync(path.resolve(fullPath), results.join('\n'));
-  // }
+  return mostRecentLead;
+}
+
+/**
+ * [getDupes description]
+ * @param  {[type]} leads [description]
+ * @param  {[type]} key   [description]
+ * @return {[type]}       [description]
+ */
+function getDupes(leads, key) {
+  var matches = {};
+  leads.forEach(function(lead1, lead1Position, leadsArray) {
+
+    leadsArray.forEach(function(lead2, lead2Position) {
+      if (lead1 !== lead2) {
+
+        if (lead1[key] === lead2[key]) {
+          if (is.not.propertyDefined(matches, lead1[key])) {
+            matches[lead1[key]] = [];
+
+            // Setup the Filter object
+            var filter = {};
+            filter[key] = lead1[key];
+
+            // Filter out the dupes
+            var filterResults = _.filter(leadsArray, filter);
+
+            // Save the filter
+            matches[lead1[key]].push(filterResults);
+          }
+        }
+      }
+    });
+
+  });
+  return matches;
+}
+
+/**
+ * [getResult description]
+ * @param  {[type]} originalLeads [description]
+ * @param  {[type]} dedupedLeads  [description]
+ * @return {[type]}               [description]
+ */
+function getResult(originalLeads, dedupedLeads) {
+  var result = originalLeads,
+      removedRecords = [];
+  dedupedLeads.forEach(function(dedupedLead) {
+
+    var originalIdMatches = _.filter(originalLeads, { _id: dedupedLead._id });
+    originalIdMatches.forEach(function(originalIdMatch) {
+      if (originalIdMatch !== dedupedLead) {
+        // delete it
+        result.splice(result.indexOf(originalIdMatch), 1);
+        removedRecords.push(originalIdMatch);
+      }
+    });
+
+    originalEmailMatches = _.filter(originalLeads, { email: dedupedLead.email });
+    originalEmailMatches.forEach(function(originalEmailMatch) {
+      if (originalEmailMatch !== dedupedLead) {
+        // delete it
+        result.splice(result.indexOf(originalEmailMatch), 1);
+        removedRecords.push(originalEmailMatch);
+      }
+    });
+  });
+  return {
+    result: result,
+    removedRecords: removedRecords
+  }
+}
+
+/**
+ * Do your shtuff
+ * @param  {[type]} fileName [description]
+ * @return {[type]}          [description]
+ */
+if (fileName) {
+
+  var leads = JSON.parse(fs.readFileSync(path.resolve(__dirname, './workspace/' + fileName)).toString()).leads,
+      source = JSON.parse(fs.readFileSync(path.resolve(__dirname, './workspace/' + fileName)).toString()).leads,
+      idDupes = getDupes(leads, '_id'),
+      emailDupes = getDupes(leads, 'email'),
+      dedupedIdLeads = dedupeLeads(idDupes),
+      dedupedEmailLeads = dedupeLeads(emailDupes)
+      dedupedLeads = _.union(dedupedIdLeads, dedupedEmailLeads),
+      result = getResult(leads, dedupedLeads);
+
+  console.log("Source: ");
+  console.log(JSON.parse(fs.readFileSync(path.resolve(__dirname, './workspace/' + fileName)).toString()).leads);
+  console.log("Number of Records: " + source.length);
+  console.log("\n");
+
+  console.log("Removed Records:");
+  console.log(result.removedRecords);
+  console.log("Number of Removed Records: " + result.removedRecords.length);
+  console.log("\n");
+
+  console.log("Result: ");
+  console.log(result.result);
+  console.log("Number of Records: " + result.result.length);
+  console.log("\n");
+
 }
